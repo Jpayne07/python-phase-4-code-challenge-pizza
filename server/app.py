b7ab5ca203241d3d486e59c3fe50f4eb532d4051
Jacob <jacobpayne007@gmail.com>
2024-12-12 07:04:41 -0600

    Finalized project

--- server/app.py
+++ server/app.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate
 from flask import Flask, request, make_response
 from flask_restful import Api, Resource
 import os
+from sqlalchemy import desc
+
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -29,13 +31,81 @@ class Restaurants(Resource):
     def get(self):
         response = make_response([{'address':restaurant.address,'id':restaurant.id,'name':restaurant.name} for restaurant in Restaurant.query.all()], 200)
         return response
+
 class IndividualRestaurant(Resource):
     def get(self, id):
-        response = make_response(Restaurant.query.filter_by(id = id).first().to_dict(), 200)
+        restaurant = Restaurant.query.filter_by(id = id).first()
+        if restaurant:
+
+            json_response = {
+                'address': restaurant.address,
+                'id': restaurant.id,
+                'name': restaurant.name,
+                'restaurant_pizzas': [
+                    {
+                        'id': rp.id,
+                        'pizza_id': rp.pizza_id,  # Include pizza_id
+                        'price': rp.price,  # Include price
+                        'restaurant_id': rp.restaurant_id,  # Include restaurant_id
+                        'pizza': {
+                            'id': rp.pizza.id,
+                            'name': rp.pizza.name,
+                            'ingredients': rp.pizza.ingredients
+                        }
+                    } for rp in restaurant.restaurant_pizzas
+                ]
+            }
+            response  = make_response(json_response, 200)
+            return response
+        else:
+            return ({'error': "Restaurant not found"}, 404)
+
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id = id).first()
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return {}, 204
+        else:
+            return {"error": "Restaurant not found"}, 404
+        
+class Pizzas(Resource):
+    def get(self):
+        response = make_response([{'id':pizza.id,'ingredients':pizza.ingredients,'name':pizza.name} for pizza in Pizza.query.all()], 200)
         return response
     
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            new_pizza = RestaurantPizza(price = data['price'], pizza_id = data['pizza_id'], restaurant_id = data['restaurant_id'])
+            print(new_pizza.to_dict())
+            if new_pizza:
+                
+                db.session.add(new_pizza)
+                db.session.commit()
+
+                restaurant_pizza_object = RestaurantPizza.query.order_by(desc(RestaurantPizza.id)).first()
+                response_item = {
+                        "id": restaurant_pizza_object.id,
+                        "pizza_id": restaurant_pizza_object.pizza_id,
+                        "price": restaurant_pizza_object.price,
+                        "restaurant_id": restaurant_pizza_object.restaurant_id,
+                        "pizza" : restaurant_pizza_object.pizza.to_dict(),
+                        "restaurant": restaurant_pizza_object.restaurant.to_dict()
+                }
+                print(restaurant_pizza_object.to_dict())
+                return  response_item, 201
+            else:
+                return {'errors': '[validation errors]'}, 404
+        except ValueError as e:
+            response = make_response({"errors": ['validation errors']}, 400)
+            return response
+
 api.add_resource(Home, '/home')
 api.add_resource(Restaurants, '/restaurants')
 api.add_resource(IndividualRestaurant, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -22,11 +22,11 @@ class Restaurant(db.Model, SerializerMixin):
 
     restaurant_pizzas = db.relationship("RestaurantPizza", back_populates = 'restaurant', cascade = 'delete')
     # add relationship
-    restaurants = association_proxy('restaurant_pizzas', 'pizza',
+    pizzas = association_proxy('restaurant_pizzas', 'pizza',
         creator = lambda pizza_obj: RestaurantPizza(pizza = pizza_obj))
 
     # add serialization rules
-    serialize_rules = ('-restaurant_pizza.restaurant', '-pizza.restaurant',)
+    serialize_rules = ('-restaurant_pizzas',)  # Exclude nested restaurant field
     def __repr__(self):
         return f"<Restaurant {self.name}>"
 
@@ -42,7 +42,7 @@ class Pizza(db.Model, SerializerMixin):
     restaurants = association_proxy('restaurant_pizzas', 'restaurant',
         creator = lambda restaurant_obj: RestaurantPizza(restaurant = restaurant_obj))
     # add serialization rules
-    serialize_rules = ('-restaurant_pizza.pizza', '-restaurant.pizza',)
+    serialize_rules = ('-restaurant_pizzas',)  # Exclude restaurant_pizzas
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,21 +50,19 @@ class Pizza(db.Model, SerializerMixin):
 
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = "restaurant_pizzas"
-
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id') )
-
     # add relationships
     pizza = db.relationship('Pizza', back_populates = 'restaurant_pizzas')
     restaurant = db.relationship('Restaurant', back_populates = 'restaurant_pizzas')
     # add serialization rules
-    serialize_rules = ('-pizza.restaurant_pizza', '-restaurant.restaurant_pizza',)
+    serialize_rules = ('-restaurant', '-pizza')  # Include pizza, exclude restaurant
     # add validation
     @validates('price')
     def price_validator(self, key, price):
-        if not 1 < price < 30:
+        if not 1 <= price <= 30:
             raise ValueError("Price must be between 1 and 30 dollars")
        
         return price
